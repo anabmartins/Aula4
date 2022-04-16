f89292e9a1de89d4bf3c26654fd33b088b3bd161
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-04-16 11:59:37 -0300

    Add files via upload

---
+++ dificeis_11-15/ex13/soma.js
@@ -0,0 +1,10 @@
+//Crie 4 variáveis e mostre ao usuário
+//a soma dos totais de caracteres;
+
+var f1 = "Melancia,"
+var f2 = " Abacate,"
+var f3 = " Laranja,"
+var f4 = " Morango."
+
+resultado = (f1)+(f2)+(f3)+(f4);
+console.log(resultado);
