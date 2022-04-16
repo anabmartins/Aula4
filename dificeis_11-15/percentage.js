21d5a6108421e67799a53f397a59ff8fc120bf06
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-04-16 09:08:08 -0300

    Delete dificeis_11-15 directory

--- dificeis_11-15/percentage.js
+++
@@ -1,6 +0,0 @@
-function calcular() {
-    resultado = document.getElementById('num').value;
-    var num = document.getElementById("num").value;
-    document.getElementById("resultado")
-.value = (num / 100) * 5;
-}
