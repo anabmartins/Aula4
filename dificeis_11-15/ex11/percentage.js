0b5d7ef82a6120ae05673a7cd56c52744c661646
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-04-16 10:07:56 -0300

    Add files via upload

---
+++ dificeis_11-15/ex11/percentage.js
@@ -0,0 +1,6 @@
+function calcular() {
+    resultado = document.getElementById('num').value;
+    var num = document.getElementById("num").value;
+    document.getElementById("resultado")
+.value = (num / 100) * 5;
+}
---
+++ dificeis_11-15/ex12/catalago.js
@@ -0,0 +1,6 @@
+function calcularBatom(){
+quantidade = document.getElementById('quantidade').value
+valor = (9.90 * parseFloat(quantidade))
+var valor = valor
+document.getElementById('valor').value = valor
+}
